44749670e4bc73a540318557d370213d7047c278
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-03-15 18:02:48 +0100

    built repo

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>php badwords</title>
+</head>
+<body>
+    <h1>ciaone</h1>
+</body>
+</html>
